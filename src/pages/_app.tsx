07f8a2413be8123525cccd149e44afb4c2825053
anh.laiviet <anh.laiviet@powergatesoftware.com>
2022-05-07 08:29:22 +0700

    fix warning useLayoutEffect in server side

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import Layout from "@/components/Layout";
 import AuthContextProvider, { useAuthContext } from "@/utils/context";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import { useLayoutEffect } from "react";
+import { useIsomorphicLayoutEffect } from "src/hooks/useIsomorphicLayoutEffect";
 import "../styles/globals.css";
 
 type AppPropsType = AppProps & {
@@ -20,7 +20,7 @@ function App(props: AppPropsType) {
 
   const router = useRouter();
 
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     if (Component.defaultProps?.protected && !isAuthenticated) {
       router.replace("/login");
     }
